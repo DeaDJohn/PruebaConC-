be1b99ea314171e0e9e773adf743f336f1e63b30
Juanjo Fernandez <juanjofb86@gmail.com>
2018-09-23 22:05:16 +0200

    Nombre y apellidos utilizando metodos.

--- PruebaCore/PruebaCore/Program.cs
+++ PruebaCore/PruebaCore/Program.cs
@@ -6,7 +6,45 @@ namespace PruebaCore
     {
         static void Main(string[] args)
         {
+            string mensaje2;
+
             Console.WriteLine("Hello World! ");
+            WriteConsole("Por favor escribe su nombre");
+            string mensaje1 = ReadConsole();
+            if (string.IsNullOrEmpty(mensaje1))
+            {
+                WriteConsole("Por favor escribe su nombre");
+                mensaje1 = ReadConsole();
+            }
+            else
+            {
+                WriteConsole("Por favor escribe su apellido");
+                mensaje2 = ReadConsole();
+                if (string.IsNullOrEmpty(mensaje2))
+                {
+
+                    WriteConsole("Por favor escribe su apellido");
+                    mensaje2 = ReadConsole();
+                }
+                else
+                {
+                    string mensajeMostrar = String.Format("Tu nombre es {0} {1}", mensaje1, mensaje2);
+                    WriteConsole(mensajeMostrar);
+                    Console.ReadKey();
+                }
+            }
+            
+            
+        }
+
+        static string ReadConsole()
+        {
+            string mensajeEscrito = Console.ReadLine();
+            return mensajeEscrito;
+        }
+        static void WriteConsole(string msg)
+        {
+            Console.WriteLine(msg);
         }
     }
 }
